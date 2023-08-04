bfc6346e39dcaf663f5e3178be4211a6f62de35c
LuisRojas <capoterojas123@gmail.com>
2023-08-04 16:52:25 -0500

    Tablas de multiplicar con Matrces

--- taller_1/src/array/BuscarElementoArray.java
+++ taller_1/src/array/BuscarElementoArray.java
@@ -1,5 +1,29 @@
 package array;
+
+import java.util.Scanner;
+
 public class BuscarElementoArray {
+
+    public static int buscarElemento(int array[], int valor) {
+        for (int x = 0; x < array.length; x++) {
+            if ((array[x]) == valor) {
+                return x;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int[] elementos = { 50, 21, 6, 97, 18 };
+        int valor = 0;
+        System.out.println("Digite el valor que desea obtener de la lista : ");
+        valor = Integer.parseInt(sc.nextLine());
+        int posicion = buscarElemento(elementos, valor);
+        if (posicion == -1) {
+            System.out.println("El elemento NO existe en el arreglo");
+        } else {
+            System.out.println("se encuentra en la posición " + posicion);
+        }
     }
 }
---
+++ taller_1/src/matriz/TablaMultiplicar.java
@@ -0,0 +1,31 @@
+package matriz;
+
+public class TablaMultiplicar {
+    static int[][] crearMatriz(int filas, int columnas) {
+        int[][] matriz = new int[filas][columnas];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = i * j;
+            }
+        }
+        return matriz;
+    }
+
+    static void muestraMatriz(int[][] matriz) {
+        int i, j;
+
+        for (i = 1; i <= matriz.length - 1; i++) {
+            for (j = 1; j <= matriz[i].length - 1; j++) {
+                System.out.println(i + " x " + j + " = " + matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[][] matriz = new int[12][12];
+
+        matriz = crearMatriz(11, 11);
+        muestraMatriz(matriz);
+    }
+}
