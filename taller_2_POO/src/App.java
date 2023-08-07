af91714902414c9cafb33cb7c9b0e41cebcbccc7
LuisRojas <capoterojas123@gmail.com>
2023-08-06 22:18:06 -0500

    Taller 2 reservas habitacion

--- taller_2_POO/src/App.java
+++ taller_2_POO/src/App.java
@@ -1,6 +1,4 @@
-import main.java.com.poo.conversormonedas.ConversorMonedas;
-import main.java.com.poo.conversormonedas.Moneda;
-import main.java.com.poo.conversormonedas.Pais;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
--- taller_2_POO/src/main/java/com/poo/alumnos/Alumno.java
+++ taller_2_POO/src/main/java/com/poo/alumnos/Alumno.java
@@ -1,6 +1,5 @@
 package main.java.com.poo.alumnos;
 
-import java.util.ArrayList;
 
 public class Alumno {
     private int id;
@@ -10,7 +9,7 @@ public class Alumno {
     public Alumno(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
-       
+
     }
 
     public String getNombre() {
@@ -29,10 +28,6 @@ public class Alumno {
         this.edad = edad;
     }
 
-    public void agregarNota(Calificacion nota) {
-        notas.add(nota);
-    }
-
     public int getId() {
         return id;
     }
@@ -41,15 +36,4 @@ public class Alumno {
         this.id = id;
     }
 
-    public double calcularPromedio() {
-        if (notas.isEmpty()) {
-            return 0;
-        } else {
-            double sum = 0;
-            for (Calificacion nota : notas) {
-                sum += nota.getValor();
-            }
-            return sum / notas.size();
-        }
-    }
 }
